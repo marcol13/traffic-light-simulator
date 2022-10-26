c28f698f1306b12e0742b9813803c6a82e6d9f6b
Marcin Krueger <marcinkrueger@gmail.com>
2022-10-26 16:59:29 +0200

    Create moveCar prototype method

--- core/src/com/put/urbantraffic/Car.java
+++ core/src/com/put/urbantraffic/Car.java
@@ -6,7 +6,27 @@ import lombok.Data;
 public class Car {
     private final Node startNode;
     private final Node endNode;
-    private final Lane startLane;
-    private final Lane endLane;
+    private Node currentNode;
+    private Node nextNode;
+//    private final Lane startLane;
+//    private final Lane endLane;
+    private int nodePercentage;
+    private final List<Node> path = new ArrayList();
     private final RideStatus status = RideStatus.WAITING;
+
+    public void moveCar(){
+        nodePercentage += 10;
+        if(nodePercentage >= 100){
+            nodePercentage %= 100;
+            path.remove(0);
+            if(path.size() > 1){
+                currentNode = path.get(0);
+                nextNode = path.get(1);
+            }
+            else{
+                //FINISH
+            }
+
+        }
+    }
 }
