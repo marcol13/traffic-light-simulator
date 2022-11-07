f520141d5896db842d3ca340bc765e2851289605
Wojtek <wojciechkrystyniak@gmail.com>
2022-11-07 20:49:18 +0100

    Update methods for getting speed limit and length

--- core/src/com/put/urbantraffic/Road.java
+++ core/src/com/put/urbantraffic/Road.java
@@ -8,4 +8,12 @@ import java.util.List;
 public class Road {
     private final int id;
     private final List<Lane> laneList;
+
+    int getSpeedLimit() {
+        return laneList.get(0).getSpeedLimit();
+    }
+
+    int getLength() {
+        return laneList.get(0).getLength();
+    }
 }
