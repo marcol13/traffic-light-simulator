ca0329094ab8a8b8286e026e3b7df1842c28fce9
Marcin <marcinkrueger@onet.eu>
2022-11-12 13:18:36 +0100

    Fix moveCar start and end lane bug

--- core/src/com/put/urbantraffic/Car.java
+++ core/src/com/put/urbantraffic/Car.java
@@ -43,16 +43,17 @@ public class Car {
 
         this.path = generatePath(startLane, endLane);
 
-        this.currentNode = path.get(0);
+        this.currentNode = new Node(path.get(0).getX(), path.get(0).getY());
         this.currentLane = lanesList.get(0);
-        this.actualNode = path.get(0);
+        this.actualNode = new Node(path.get(0).getX(), path.get(0).getY());
         this.nextNode = path.get(1);
 
         this.way = calculateWay(this.currentNode, this.nextNode);
 
         System.out.println(this.path);
     }
-    private List<Node> generatePath(Lane startLane, Lane endLane){
+
+    private List<Node> generatePath(Lane startLane, Lane endLane) {
         calculatedPath = UrbanTrafficFlowSimulation.paths[startLane.getId()][endLane.getId()];
         this.crossingList = calculatedPath.getCrossings();
 
@@ -63,12 +64,12 @@ public class Car {
         System.out.println(calculatedPath);
 
         List<Node> path = new ArrayList<>(Collections.singletonList(this.startNode));
-        if(this.crossingList.size() == 1)
+        if (this.crossingList.size() == 1)
             path.add(new Node(this.crossingList.get(0).getX(), this.crossingList.get(0).getY()));
         else
-            for(Lane lane: calculatedPath.getLanes()){
-                for(Node node: lane.getNodeList()){
-                    if(!node.equals(path.get(path.size() - 1))){
+            for (Lane lane : calculatedPath.getLanes()) {
+                for (Node node : lane.getNodeList()) {
+                    if (!node.equals(path.get(path.size() - 1))) {
                         path.add(node);
                     }
                 }
@@ -81,12 +82,25 @@ public class Car {
 
     public void moveCar() {
         if (status != RideStatus.FINISH) {
-            nodePercentage += currentLane.getSpeedLimit() / 100.0;
+            ArrayList<Node> nodeList = new ArrayList<>(currentLane.getNodeList());
+            float speed = (float)currentLane.getSpeedLimit() / (float)getNodeLength(nodeList.get(0), nodeList.get(nodeList.size() - 1));
+
+            int xVector = nextNode.getX() - currentNode.getX();
+            int yVector = nextNode.getY() - currentNode.getY();
+
+            if(currentNode.equals(startNode) || nextNode.equals(endNode))
+                speed *= 2;
+
+            nodePercentage += speed;
 
             if (nodePercentage >= 100) {
                 nodePercentage %= 100;
                 path.remove(0);
-                lanesList.remove(0);
+
+                if(currentLane.getNodeList().size() == 2 || currentLane.getNodeList().size() > 2 && currentNode == currentLane.getNodeList().get(2)){
+                    lanesList.remove(0);
+                }
+
                 if (path.size() > 1) {
                     currentNode = path.get(0);
                     currentLane = lanesList.get(0);
@@ -98,20 +112,23 @@ public class Car {
                     return;
                 }
             }
-
-            actualNode.setX((int)(currentNode.getX() + (nextNode.getX() - currentNode.getX()) * nodePercentage / 100));
-            actualNode.setY((int)(currentNode.getY() + (nextNode.getY() - currentNode.getY()) * nodePercentage / 100));
+            actualNode.setX((int) (currentNode.getX() + xVector * nodePercentage / 100));
+            actualNode.setY((int) (currentNode.getY() + yVector * nodePercentage / 100));
         }
     }
 
+    private int getNodeLength(Node currentNode, Node nextNode){
+        return Math.abs(currentNode.getX() - nextNode.getX()) + Math.abs(currentNode.getY() - nextNode.getY());
+    }
+
     private Way calculateWay(Node startNode, Node endNode) {
-        if(startNode.getX() == endNode.getX()){
-            if(startNode.getY() > endNode.getY())
+        if (startNode.getX() == endNode.getX()) {
+            if (startNode.getY() > endNode.getY())
                 return Way.BOTTOM;
             return Way.TOP;
         }
 
-        if(startNode.getX() > endNode.getX())
+        if (startNode.getX() > endNode.getX())
             return Way.LEFT;
         return Way.RIGHT;
     }
