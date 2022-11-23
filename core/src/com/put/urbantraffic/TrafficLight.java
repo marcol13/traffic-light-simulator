ea96c775f436b14c29c9f39a9079661b41aa94a1
PawelZ4krzewski <61842320+PawelZ4krzewski@users.noreply.github.com>
2022-11-23 17:16:53 +0100

    Add @AllArgsConstructor adnotation in Traffic Light

--- core/src/com/put/urbantraffic/TrafficLight.java
+++ core/src/com/put/urbantraffic/TrafficLight.java
@@ -1,9 +1,11 @@
 package com.put.urbantraffic;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 
 @Data
+@AllArgsConstructor
 public class TrafficLight {
     private Lane startLane;
     private Light currentColor;
