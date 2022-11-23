05e7250edafa59deb2e14da7c5df3eb3db57264c
PawelZ4krzewski <61842320+PawelZ4krzewski@users.noreply.github.com>
2022-11-23 16:51:30 +0100

    remove unnecessary addnotation in TrafficLight

--- core/src/com/put/urbantraffic/TrafficLight.java
+++ core/src/com/put/urbantraffic/TrafficLight.java
@@ -1,11 +1,9 @@
 package com.put.urbantraffic;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
 
 @Data
-@AllArgsConstructor
 public class TrafficLight {
     private Lane startLane;
     private Light currentColor;
